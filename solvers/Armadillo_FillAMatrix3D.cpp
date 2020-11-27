ManIVIctorious/TheNumerov

commit 85ea3f46bcbfe28de18720f5b38271a2a65c4ec8

Code cleanup of Solvers
    * Instead of passing the whole settings struct to the individual
      solver routines only pass the struct pointer
    * Move solvers, fillers and accompanying files to "solvers" directory

    * General code clean-up of the individual filling routines.
      * Do a proper calculation of the number of entries in the A matrix
        instead of relying on an upper estimate (MKL) or a first
        counting run applying a threshold (Armadillo).
        In case of the MKL implementation this is both more memory
        efficient and faster, while it actually uses more memory in case
        of the Armadillo routine (due to the previously applied
        threshold of 1.0E-8) but should be more accurate.
      * Make Armadillo and MKL filling routines more similar
      * Align code more to project's coding standards

    * Fix a logic error in the application of the molecular Hamiltonian
      rotation terms.
      Due to the symmetry of the derivative stencils as well as the
      commutativity of the multiplication operation the results are
      equivalent, although the indices were previously switched.

    It is notable though, that the Armadillo 3D code has not been tested yet.

make.src
@@ -44,22 +44,22 @@
 # GSL sources
 #----------------------------------------------------------------------
 ifeq ($(findstring HAVE_GSL_INSTALLED, $(PPF)), HAVE_GSL_INSTALLED)
-  GSLSRC += GSL/GSL_GenFiniteDifferenceStencils.c
+  GSLSRC += solvers/GSL_GenFiniteDifferenceStencils.c
 endif
 
 # MKL sources
 #----------------------------------------------------------------------
 ifeq ($(findstring HAVE_MKL_INSTALLED, $(PPF)), HAVE_MKL_INSTALLED)
-  MKLSRC += dispatchers/MKL_SolverFEAST.c
-  MKLSRC += dispatchers/1D/MKL_Fill_1D.c
-  MKLSRC += dispatchers/2D/MKL_Fill_2D.c
+  MKLSRC += solvers/MKL_SolverFEAST.c
+  MKLSRC += solvers/MKL_FillAMatrix1D.c
+  MKLSRC += solvers/MKL_FillAMatrix2D.c
 endif
 
 # Armadillo sources (require C++)
 #----------------------------------------------------------------------
 ifeq ($(findstring HAVE_ARMA_INSTALLED, $(PPF)), HAVE_ARMA_INSTALLED)
-  ARMASRC += dispatchers/Armadillo_SolverARPACK.cpp
-  ARMASRC += dispatchers/1D/Armadillo_Fill_1D.cpp
-  ARMASRC += dispatchers/2D/Armadillo_Fill_2D.cpp
-  ARMASRC += dispatchers/3D/Armadillo_Fill_3D.cpp
+  ARMASRC += solvers/Armadillo_SolverARPACK.cpp
+  ARMASRC += solvers/Armadillo_FillAMatrix1D.cpp
+  ARMASRC += solvers/Armadillo_FillAMatrix2D.cpp
+  ARMASRC += solvers/Armadillo_FillAMatrix3D.cpp
 endif

solvers/Armadillo_FillAMatrix3D.cpp
@@ -1,0 +1,67 @@
+
+#include <stdio.h>
+#include <armadillo>
+#include "settings.h"
+
+// Provided Prototypes
+arma::sp_mat FillArmadillo_3D(settings* prefs, int* nq, int n_points, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta);
+
+
+// 3D fill
+arma::sp_mat FillArmadillo_3D(settings* prefs, int* nq, int n_points, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta){
+
+// Calculate the maximum number of non-zero entries in the A matrix
+//  Should be <n_points - (n_stencil/2)*2> lines with <n_stencil> entries, the
+//  first and last <n_stencil/2> lines are shortened via a triangular number sequence
+    int max_entries = (prefs->n_stencil*nq[0] - (prefs->n_stencil/2)*(prefs->n_stencil/2 + 1))
+                     *(prefs->n_stencil*nq[1] - (prefs->n_stencil/2)*(prefs->n_stencil/2 + 1))
+                     *(prefs->n_stencil*nq[2] - (prefs->n_stencil/2)*(prefs->n_stencil/2 + 1));
+
+
+// determine positions and values
+    arma::umat locations(2, max_entries);
+    arma::vec  values = arma::zeros(max_entries);
+    unsigned int index = 0;
+
+    for(int i = 0; i < nq[0]; ++i){
+        for(int j = 0; j < nq[1]; ++j){
+            for(int k = 0; k < nq[2]; k++){
+
+                for(int xsh = -(prefs->n_stencil/2); xsh <= (prefs->n_stencil/2); ++xsh){
+                if( (i + xsh > -1) && (i + xsh < nq[0]) ){
+
+                    for(int ysh = -(prefs->n_stencil/2); ysh <= (prefs->n_stencil/2); ++ysh){
+                    if( (j + ysh > -1) && (j + ysh < nq[1]) ){
+
+                        for(int zsh = -(prefs->n_stencil/2); zsh <= (prefs->n_stencil/2); ++zsh){
+                        if( (k + zsh > -1) && (k + zsh < nq[2]) ){
+
+                            int xsidx = xsh + prefs->n_stencil/2;
+                            int ysidx = ysh + prefs->n_stencil/2;
+                            int zsidx = zsh + prefs->n_stencil/2;
+
+                        // locations of stencil values around the main diagonal
+                            locations (0, index) = (    i     *nq[1] +    j      )*nq[2] +    k;
+                            locations (1, index) = ( (i + xsh)*nq[1] + (j + ysh) )*nq[2] + (k + zsh);
+                        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                            values(index) = ekin_param * stencil[ (xsidx*prefs->n_stencil + ysidx*prefs->n_stencil)*prefs->n_stencil + zsidx ] / 4.0;
+                            ++index;
+                        }}
+                    }}
+                }}
+
+            }
+        }
+    }
+
+// actual sparse matrix fill
+    arma::sp_mat A(locations, values, n_points, n_points, true, true);
+
+// add potential value
+    for(int i = 0; i < n_points; ++i){
+        A(i,i) += v[i];
+    }
+
+    printf("Matrix created, Potential added, %u entries\n", index);
+    return A;
+}

solvers/MKL_SolverFEAST.c
@@ -6,20 +6,18 @@
 #include "settings.h"
 
 // Dependencies (with rotation)
-int FillMKL_1D(double* v, int* nq, double ekin_param, double* stencil, int n_stencil, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
-int FillMKL_2D(settings prefs, int* nq, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
+int MKL_FillAMatrix1D(double* v, int* nq, double ekin_param, double* stencil, int n_stencil, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
+int MKL_FillAMatrix2D(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
 
 // provided prototypes
-int SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta);
+int SolverFEAST_MKL(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta);
 
 
-int SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta){
-
-    int i;
-    int n_points;
+int SolverFEAST_MKL(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta){
 
 // get number of points
-    for(i = 0, n_points = 1; i < prefs.dimension; ++i){
+    int n_points = 1;
+    for(int i = 0; i < prefs->dimension; ++i){
         n_points *= nq[i];
     }
 
@@ -29,17 +27,17 @@ int SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, doubl
     double  * vals_A = NULL;
 
 // depending on the dimensionality use the appropriate filling routine
-    switch(prefs.dimension){
+    switch( prefs->dimension ){
 
         case 1:
         // one dimensional filling routine, the rotation terms are already set in main (Watson term)
-            FillMKL_1D(v, nq, ekin_param, stencil, prefs.n_stencil, &rows_A, &cols_A, &vals_A);
+            MKL_FillAMatrix1D(v, nq, ekin_param, stencil, prefs->n_stencil, &rows_A, &cols_A, &vals_A);
             break;
 
         case 2:
         // two dimensional filling routine, the second term of the Watson Hamiltonian is set in FillMKL
         //  when a Coriolis file is given, the third term is already set in main (Watson term)
-            FillMKL_2D(prefs, nq, v, ekin_param, stencil, q, dq, mu, zeta, &rows_A, &cols_A, &vals_A);
+            MKL_FillAMatrix2D(prefs, nq, v, ekin_param, stencil, q, dq, mu, zeta, &rows_A, &cols_A, &vals_A);
             break;
 
         default:
@@ -47,7 +45,7 @@ int SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, doubl
                 "\n (-) Error: The requested MKL FEAST filling routine"
                 "\n     for a dimensionality of \"%d\" is not implemented"
                 "\n     Please check your input. Aborting...\n\n"
-                ,prefs.dimension
+                ,prefs->dimension
             );
             exit(EXIT_FAILURE);
     }
@@ -55,21 +53,21 @@ int SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, doubl
 // allocate memory for eigenvalues E and eigenvectors X
     (*E) = malloc(n_points          * sizeof(double));
     (*X) = malloc(n_points*n_points * sizeof(double));
-    if((*E) == NULL){ perror("Eigenvalues" ); exit(errno); }
-    if((*X) == NULL){ perror("Eigenvectors"); exit(errno); }
+    if( (*E) == NULL ){ perror("Eigenvalues" ); exit(errno); }
+    if( (*X) == NULL ){ perror("Eigenvectors"); exit(errno); }
 
 // Start eigenstate calculation
     double * res = calloc(n_points, sizeof(double));    // Residual
-    if(res == NULL){ perror("SolverFEAST_MKL \"residue\""); exit(errno); }
+    if( res == NULL ){ perror("SolverFEAST_MKL \"residue\""); exit(errno); }
 
     char           UPLO     = 'F';
-    const MKL_INT  N        = n_points;
-    MKL_INT        fpm[128] = { 0 };        // Array to pass parameters to Intel MKL Extended Eigensolvers
-    double         epsout   = 0.0;          // Relative error on the trace
-    MKL_INT        loop     = 0;            // Number of refinement loop
-    MKL_INT        M0       = n_points/2;   // Initial guess for subspace dimension to be used
-    MKL_INT        n_out    = n_points/2;
-    MKL_INT        info     = 0;            // Errors
+    const MKL_INT  N        = (MKL_INT)n_points;
+    MKL_INT        fpm[128] = { 0 };                // Array to pass parameters to Intel MKL Extended Eigensolvers
+    double         epsout   = 0.0;                  // Relative error on the trace
+    MKL_INT        loop     = 0;                    // Number of refinement loop
+    MKL_INT        M0       = (MKL_INT)n_points/2;  // Initial guess for subspace dimension to be used
+    MKL_INT        n_out    = (MKL_INT)n_points/2;
+    MKL_INT        info     = 0;                    // Errors
 
     feastinit(fpm); // Initialize Eigensolver parameter array
 
@@ -82,8 +80,8 @@ int SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, doubl
         fpm,            // IN/OUT: Array is used to pass parameters to Intel MKL Extended Eigensolvers
         &epsout,        // OUT: Relative error of on the trace
         &loop,          // OUT: Contains the number of refinement loop executed
-        &prefs.e_min,   // IN: Lower bound of search interval
-        &prefs.e_max,   // IN: Upper bound of search interval
+        &prefs->e_min,  // IN: Lower bound of search interval
+        &prefs->e_max,  // IN: Upper bound of search interval
         &M0,            // IN: The initial guess for subspace dimension to be used.
         (*E),              // OUT: The first M entries of Eigenvalues
         (*X),              // IN/OUT: The first M entries of Eigenvectors
@@ -93,23 +91,23 @@ int SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, doubl
     );
 
 // Error output
-    if( info != 0 ){
+    if( info ){
         fprintf(stderr,
             "\n (-) Error, \"sfeast_scsrev\" returned code of error: %d"
             "\n     Aborting...\n\n"
             , (int)info
         );
-        exit((int)info);
+        exit( (int)info );
     }
 
 // reallocate to actual required memory amount
     (*E) = realloc( (*E), n_out          * sizeof(double));
     (*X) = realloc( (*X), n_out*n_points * sizeof(double));
-    if((*E) == NULL){ perror("Eigenvalues" ); exit(errno); }
-    if((*X) == NULL){ perror("Eigenvectors"); exit(errno); }
+    if( (*E) == NULL ){ perror("Eigenvalues" ); exit(errno); }
+    if( (*X) == NULL ){ perror("Eigenvectors"); exit(errno); }
 
 
-// free memory of arrays which are not needed anymore
+// Garbage Collection
     free(res);      res     = NULL;
     free(rows_A);   rows_A  = NULL;
     free(cols_A);   cols_A  = NULL;

MetaEigensolver.c
@@ -5,21 +5,21 @@
 
 // Dependencies
 #ifdef HAVE_MKL_INSTALLED
-int        SolverFEAST_MKL(settings prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta);
+int        SolverFEAST_MKL(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta);
 #endif
 #ifdef HAVE_ARMA_INSTALLED
-int SolverARPACK_Armadillo(settings prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta);
+int SolverARPACK_Armadillo(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta);
 #endif
 
 // provided prototypes
-int MetaEigensolver(settings prefs, int* nq, double* v, double ekin_param, double* stencil, double* *E, double* *X, double** q, double dq, double*** mu, double** zeta);
+int MetaEigensolver(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double* *E, double* *X, double** q, double dq, double*** mu, double** zeta);
 
 
-int MetaEigensolver(settings prefs, int* nq, double* v, double ekin_param, double* stencil, double* *E, double* *X, double** q, double dq, double*** mu, double** zeta){
+int MetaEigensolver(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double* *E, double* *X, double** q, double dq, double*** mu, double** zeta){
 
     int n_out;
 
-    switch(prefs.Eigensolver){
+    switch( prefs->Eigensolver ){
 
 #ifdef HAVE_MKL_INSTALLED
         case 1:

dispatchers/1D/Armadillo_Fill_1D.cpp
@@ -1,62 +1,0 @@
-
-#include <stdio.h>
-#include <armadillo>
-#include "settings.h"
-
-// Provided Prototypes
-arma::sp_mat FillArmadillo_1D(int* nq, double* v, double ekin_param, double* stencil, int n_stencil);
-
-
-// 1D fill
-arma::sp_mat FillArmadillo_1D(int* nq, double* v, double ekin_param, double* stencil, int n_stencil){
-
-    int i;
-    int xsh;
-    int max_entries = 0;
-    double threshold = 1.0E-8;
-
-// determine maximum number of entries (number of stencil entries inside the matrix greater than threshold)
-    for(i = 0; i < nq[0]; ++i){
-
-        for(xsh = -n_stencil/2; xsh <= n_stencil/2; ++xsh){
-        if( ( (i+xsh) > -1 ) && ( (i+xsh) < nq[0] ) ){
-
-            if( stencil[xsh + n_stencil/2] > threshold || stencil[xsh + n_stencil/2] < threshold ){
-                max_entries++;
-            }
-
-        }}
-    }
-
-// determine positions and values
-    arma::umat locations(2, max_entries);
-    arma::vec  values = arma::zeros(max_entries);
-    unsigned int index = 0;
-
-    for(i = 0; i < nq[0]; ++i){
-
-        for(xsh = -n_stencil/2; xsh <= n_stencil/2; ++xsh){
-        if( ( (i+xsh) > -1 ) && ( (i+xsh) < nq[0] ) ){
-
-            if( stencil[xsh + n_stencil/2] > threshold || stencil[xsh + n_stencil/2] < threshold ){
-            // locations of stencil values around the main diagonal
-                locations(0, index) =     i;       // rows
-                locations(1, index) = ( i + xsh ); // columns
-            //++++++++++++++++++++++++++++++++++++++++++++++++++
-                values(index++) = stencil[xsh + n_stencil/2] * ekin_param;
-
-            }
-        }}
-    }
-
-// actual sparse matrix fill
-    arma::sp_mat A(locations, values, nq[0], nq[0], true, true);
-
-// add potential values to main diagonal
-    for(i = 0; i < nq[0]; ++i){
-        A(i,i) += v[i];
-    }
-
-    printf("Matrix created, Potential added, %u entries\n", index);
-    return A;
-}

commit 3d18e53f768a3959a82ba24aa7be2a239a8c2b17

Fix critical bug in Armadillo 3D and add MKL 3D
    The bug was introduced in 85ea3f46bcbfe28de18720f5b38271a2a65c4ec8
    when a general cleanup of the filling routines was performed.
    When collapsing the stencil index into parenthesis the deletion of
    an instance of prefs->n_stencil was omitted.

    Add implementation of MKL_FillAMatrix3D()
    Due to the limitations of the MKL Extended Eigensolver API requiring
    a pre-allocation of the eigen value and eigen vector arrays the 3D
    problem requires an enormous amount of memory before execution.
    Hence, hardware limitations prevented the application to real world
    data.

make.def
@@ -13,6 +13,7 @@ OPT = -g -Og
   LIB      = -lm
   INC      = -Iinclude/
   PACKAGES = gsl mkl armadillo
+  WARN += -Wno-unused-parameter
 
 # Preprocessor flags
   PPF += -D HAVE_GSL_INSTALLED  # gnu scientific library support

make.src
@@ -54,6 +54,7 @@ ifeq ($(findstring HAVE_MKL_INSTALLED, $(PPF)), HAVE_MKL_INSTALLED)
   MKLSRC += solvers/MKL_SolverFEAST.c
   MKLSRC += solvers/MKL_FillAMatrix1D.c
   MKLSRC += solvers/MKL_FillAMatrix2D.c
+  MKLSRC += solvers/MKL_FillAMatrix3D.c
 endif
 
 # Armadillo sources (require C++)

solvers/Armadillo_FillAMatrix3D.cpp
@@ -25,7 +25,7 @@ arma::sp_mat FillArmadillo_3D(settings* prefs, int* nq, int n_points, double* v,
 
     for(int i = 0; i < nq[0]; ++i){
         for(int j = 0; j < nq[1]; ++j){
-            for(int k = 0; k < nq[2]; k++){
+            for(int k = 0; k < nq[2]; ++k){
 
                 for(int xsh = -(prefs->n_stencil/2); xsh <= (prefs->n_stencil/2); ++xsh){
                 if( (i + xsh > -1) && (i + xsh < nq[0]) ){
@@ -40,11 +40,11 @@ arma::sp_mat FillArmadillo_3D(settings* prefs, int* nq, int n_points, double* v,
                             int ysidx = ysh + prefs->n_stencil/2;
                             int zsidx = zsh + prefs->n_stencil/2;
 
-                        // locations of stencil values around the main diagonal
+                        // locations of stencil values (rows 0 and columns 1)
                             locations (0, entry_index) = (    i     *nq[1] +    j      )*nq[2] +    k;
                             locations (1, entry_index) = ( (i + xsh)*nq[1] + (j + ysh) )*nq[2] + (k + zsh);
                         //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-                            values(entry_index) = ekin_param * stencil[ (xsidx*prefs->n_stencil + ysidx*prefs->n_stencil)*prefs->n_stencil + zsidx ] / 4.0;
+                            values(entry_index) = ekin_param * stencil[ (xsidx*prefs->n_stencil + ysidx)*prefs->n_stencil + zsidx ] / 4.0;
                             ++entry_index;
                         }}
                     }}

solvers/MKL_FillAMatrix3D.c
@@ -1,0 +1,100 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <mkl_solvers_ee.h>
+
+#include "settings.h"
+
+// Dependencies
+void   init_watson_3d(settings* prefs);
+double exec_watson_3d(double*** mu, double** zeta, int* nq, double dq, double** q, int i, int j, int xsidx, int ysidx);
+void   free_watson_3d(void);
+
+// provided prototypes
+int MKL_FillAMatrix3D(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
+
+
+// 2D fill
+int MKL_FillAMatrix3D(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A){
+
+// Calculate the maximum number of non-zero entries in the A matrix
+//  Should be <n_points - (n_stencil/2)*2> lines with <n_stencil> entries, the
+//  first and last <n_stencil/2> lines are shortened via a triangular number sequence
+    int n_points = nq[0] * nq[1] * nq[2];
+    int max_entries = (prefs->n_stencil*nq[0] - (prefs->n_stencil/2)*(prefs->n_stencil/2 + 1))
+                     *(prefs->n_stencil*nq[1] - (prefs->n_stencil/2)*(prefs->n_stencil/2 + 1))
+                     *(prefs->n_stencil*nq[2] - (prefs->n_stencil/2)*(prefs->n_stencil/2 + 1));
+
+// The sparse matrix eigensolver of Intel MKL saves the positions of non empty entries in
+//  two integer arrays (rows_A & cols_A), each non zero entry increments the counter by 1
+//  rows_A contains the counter value of each first element in a line -> max entries n_points (+1 because Intel...)
+//  cols_A contains the counter values of all non zero elements -> max entries n_stencil*n_points - borders
+//  vals_A contains the values of all non zero entries -> max entries n_stencil*n_points - borders
+    (*rows_A) = calloc(n_points + 1, sizeof(MKL_INT));
+    (*cols_A) = calloc(max_entries,  sizeof(MKL_INT));
+    (*vals_A) = calloc(max_entries,  sizeof(double));
+
+    if( (*rows_A) == NULL ){ perror("3D MKL Fill rows_A"); exit(errno); }
+    if( (*cols_A) == NULL ){ perror("3D MKL Fill cols_A"); exit(errno); }
+    if( (*vals_A) == NULL ){ perror("3D MKL Fill vals_A"); exit(errno); }
+
+
+//// initialise for the calculation of the Watson Hamiltonian
+//    if( prefs->coriolis_file ){ init_watson_3d(prefs); }
+
+// fill Numerov's A matrix
+//  determine the non zero elements and store their positions in rows_A and cols_A
+//  and their values in vals_A
+    (*rows_A)[0] = 1;
+    int entry_index = 0;
+
+    for(int i = 0; i < nq[0]; ++i){
+        for(int j = 0; j < nq[1]; ++j){
+            for(int k = 0; k < nq[2]; ++k){
+
+                for(int xsh = -(prefs->n_stencil/2); xsh <= (prefs->n_stencil/2); ++xsh){
+                if( (i + xsh > -1) && (i + xsh < nq[0]) ){
+
+                    for(int ysh = -(prefs->n_stencil/2); ysh <= (prefs->n_stencil/2); ++ysh){
+                    if( (j + ysh > -1) && (j + ysh < nq[1]) ){
+
+                        for(int zsh = -(prefs->n_stencil/2); zsh <= (prefs->n_stencil/2); ++zsh){
+                        if( (k + zsh > -1) && (k + zsh < nq[2]) ){
+
+                            int xsidx = xsh + prefs->n_stencil/2;    // stencil x index
+                            int ysidx = ysh + prefs->n_stencil/2;    // stencil y index
+                            int zsidx = zsh + prefs->n_stencil/2;    // stencil z index
+
+                        // set column index
+                            (*cols_A)[entry_index] = ( (i + xsh)*nq[1] + (j + ysh) )*nq[2] + (k + zsh) + 1;
+
+                        // set matrix value
+                            (*vals_A)[entry_index] = ekin_param * stencil[ (xsidx*prefs->n_stencil + ysidx)*prefs->n_stencil + zsidx ];
+//                        //  apply second term of Watson Hamiltonian
+//                            if( prefs->coriolis_file ){
+//                                (*vals_A)[entry_index] -= exec_watson_3d(mu, zeta, nq, dq, q, i, j, xsidx, ysidx);
+//                            }
+                        //  The stencil values have to be divided by a factor of four
+                            (*vals_A)[entry_index] *= 0.25;
+
+                        // add potential to diagonal element
+                            if( (xsh == 0) && (ysh == 0) && (zsh == 0) ){
+                                (*vals_A)[entry_index] += v[ (i*nq[1] + j)*nq[2] + k ];
+                            }
+                            ++entry_index;
+                        }}
+                    }}
+                }}
+            // after inserting all entries in a row the total number of entries is inserted in the CSR format.
+                (*rows_A)[ (i*nq[1] + j)*nq[2] + k + 1] = entry_index + 1;
+            }
+        }
+    }
+
+//// free memory
+//    if( prefs->coriolis_file ){ free_watson_3d(); }
+
+    printf("Matrix created, Potential added, %d entries\n", entry_index);
+    return 0;
+}

solvers/MKL_SolverFEAST.c
@@ -8,6 +8,7 @@
 // Dependencies (with rotation)
 int MKL_FillAMatrix1D(double* v, int* nq, double ekin_param, double* stencil, int n_stencil, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
 int MKL_FillAMatrix2D(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
+int MKL_FillAMatrix3D(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** q, double dq, double*** mu, double** zeta, MKL_INT* *rows_A, MKL_INT* *cols_A, double* *vals_A);
 
 // provided prototypes
 int SolverFEAST_MKL(settings* prefs, int* nq, double* v, double ekin_param, double* stencil, double** E, double** X, double** q, double dq, double*** mu, double** zeta);
@@ -38,6 +39,18 @@ int SolverFEAST_MKL(settings* prefs, int* nq, double* v, double ekin_param, doub
         // two dimensional filling routine, the second term of the Watson Hamiltonian is set in the filling
         //  routine if a Coriolis file is given, the third term is already set in main (Watson term)
             MKL_FillAMatrix2D(prefs, nq, v, ekin_param, stencil, q, dq, mu, zeta, &rows_A, &cols_A, &vals_A);
+            break;
+
+        case 3:
+        // three dimensional filling routine:
+        //  At the moment only the basic Hamiltonian is supported.
+            if( prefs->coriolis_file ){
+                fprintf(stderr, "At the moment only the basic Hamiltonian is implemented for this"
+                                "\n3D problem. Nevertheless, the third term of the Watson Hamiltonian"
+                                "\nis already set in main() => be prepared for some wrong results!\n\n"
+                       );
+            }
+            MKL_FillAMatrix3D(prefs, nq, v, ekin_param, stencil, q, dq, mu, zeta, &rows_A, &cols_A, &vals_A);
             break;
 
         default:
@@ -83,8 +96,8 @@ int SolverFEAST_MKL(settings* prefs, int* nq, double* v, double ekin_param, doub
         &prefs->e_min,  // IN: Lower bound of search interval
         &prefs->e_max,  // IN: Upper bound of search interval
         &M0,            // IN: The initial guess for subspace dimension to be used.
-        (*E),              // OUT: The first M entries of Eigenvalues
-        (*X),              // IN/OUT: The first M entries of Eigenvectors
+        (*E),           // OUT: The first M entries of Eigenvalues
+        (*X),           // IN/OUT: The first M entries of Eigenvectors
         &n_out,         // OUT: The total number of eigenvalues found in the interval
         res,            // OUT: The first n_out components contain the relative residual vector
         &info           // OUT: Error code